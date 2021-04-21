a03531279d016804610f36fd5ce4d4928fb7e4b9
steadfasterX <steadfasterX@users.noreply.github.com>
2021-04-21 09:49:50 +0200

    Create clean.sh

---
+++ common/clean.sh
@@ -0,0 +1,49 @@
+export HOME=${WORKSPACE}
+echo "--------- home set"
+
+# get global vars
+[ -z $envfile ] && "echo missing envfile param" && exit 3
+test -f $envfile
+source $envfile
+
+cd $srcpath
+
+SLEEP=1
+prebuilts/sdk/tools/jack-admin kill-server || SLEEP=0
+jack-admin kill-server || SLEEP=0
+
+# all problematic directories (when switching between android versions)
+DELDIRS="device/
+kernel/
+vendor/lineage
+vendor/fdroid
+vendor/cm
+hardware/qcom/sm8150/
+hardware/qcom-caf
+vendor/nxp/opensource/sn100x/
+vendor/nxp/opensource/pn5xx/
+hardware/qcom/Android.mk
+"
+
+for deldir in $DELDIRS;do
+    [ "$deldir" == "./.repo" ] && echo REPO IN DELDIR && exit 99
+    [ "$deldir" == ".repo" ] && echo REPO IN DELDIR && exit 99
+    [ "$deldir" == "./user-keys" ] && echo KEYS IN DELDIR && exit 99
+    [ "$deldir" == "user-keys" ] && echo KEYS IN DELDIR && exit 99
+    [ -d $deldir ] && rm -rvf $deldir
+done
+
+# delete the symlink or dir
+[ -L out ] && rm out
+[ -d out ] && rm -rf out
+
+# clean the real out dir
+[ -d  /ssd/${PREFIXFNAME}/out ] && rm -rf  /ssd/${PREFIXFNAME}/out
+
+# create the real out dir
+mkdir -p /ssd/${PREFIXFNAME}/out
+
+# symlink out to the real out dir
+ln -s /ssd/${PREFIXFNAME}/out out
+
+ls -la out
